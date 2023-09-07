91745e8800f753541e8032b2f5437352295c2fdf
laboratorio <mpena20@alumnos.utalca.cl>
2023-09-07 10:47:51 -0300

    Second commit

--- app/src/main/java/cl/mankeke/bopem/AboutActivity.kt
+++ app/src/main/java/cl/mankeke/bopem/AboutActivity.kt
@@ -3,11 +3,9 @@ package cl.mankeke.bopem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_about)
     }
-
-
 }
